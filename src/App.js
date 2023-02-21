a0af56988218c75264e7651046a4e00c611119e2
erhanatalan <erhanatalan2013@gmail.com>
2023-02-21 14:20:49 +0300

    app js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
-import Navbar from "./components/Navbar/Navbar";
+import data from "./Data/data"
+import styled from "styled-components";
+import imgg from "./Data/nbaa.jpg"
 
+
+const DivStyles = styled.div` 
+  background-image:url(${imgg});
+  // background-image:url("https://c4.wallpaperflare.com/wallpaper/596/179/749/basketball-4k-nba-michael-jordan-wallpaper-preview.jpg");
+  // background-position: -100px;
+  background-repeat:no-repeat;
+  background-size:cover;
+  background-attachment: fixed;
+
+`;
 function App() {
   return (
-    <>
-      <Navbar/>
-      <Main/>
-    </>
+    <DivStyles className="App">
+      <Header />
+      <Main data={data}/>
+    </DivStyles>
   );
 }
 
--- src/Data/data.js
+++ src/Data/data.js
@@ -1,94 +1,124 @@
-export const data = [
+const data = [
   {
-    id: 1,
-    title: "New York",
-    desc:
-      "Home to the Statue of Liberty, Empire State Building, Central Park, Times Square, New York is the most populous city in the USA. We have a lot to tell about the city where city lights illuminate the sky.",
-    image:
-      "https://cdn.pixabay.com/photo/2017/06/07/15/47/new-york-city-2380683_960_720.jpg",
+    name: "Kareem Abdul-Jabbar",
+    img: "https://efemeridesdoefemello.files.wordpress.com/2014/05/13jun14.jpeg?w=640",
+    statistics: [
+      "38,387 points",
+      "17,440 rebaunds",
+      "5,660 assist",
+      "19 All Star",
+    ],
   },
   {
-    id: 2,
-    title: "Philippines",
-    desc:
-      "The island country, the Philippines, is one of the most beautiful countries where blue and green are together. Forget the potion of youth, traveling to the Philippines will rejuvenate you for at least 5 years.",
-    image:
-      "https://bigseventravel.com/wp-content/uploads/2019/11/john-hernandez-IL-fBBnF-GU-unsplash-1920x1440.jpg",
+    name: "Larry Bird",
+    img: "https://www.gannett-cdn.com/presto/2021/06/30/PIND/4c8d3b5f-4f15-4a3b-81d8-6b929d95b91f-larrybird.jpeg?width=300&height=455&fit=crop&format=pjpg&auto=webp",
+    statistics: [
+      "21,791 points",
+      "8,974 rebaunds",
+      "5,695 assist",
+      "12 All Star",
+    ],
   },
   {
-    id: 3,
-    title: "Bali",
-    desc:
-      "Bali, Indonesia's most famous island, has taken its place as the 'last paradise' in mind. Providing 80% of its livelihood with tourism, Bali hosts thousands of tourists every year.",
-    image: "https://ak.picdn.net/shutterstock/videos/26368511/thumb/1.jpg",
+    name: "Wilt Chamberlain",
+    img: "https://pbs.twimg.com/media/EQwzU_RXUAAAATG?format=jpg&name=4096x4096",
+    statistics: [
+      "31,419 points",
+      "23,924 rebaunds",
+      "4,643 assist",
+      "13 All Star",
+    ],
   },
   {
-    id: 4,
-    title: "Egypt",
-    desc:
-      "Being one of the biggest countries of the Middle East, Egypt has been the transition point of many civilizations with its thousands of years of history. The pyramids and miracles that come to mind when talking about Egypt have been the subject of many films. It still remains a mystery.",
-    image:
-      "https://images.memphistours.com/large/34d5b5a3fbaa4b3b5d9487bf924b0145.jpg",
+    name: "Magic Johnson",
+    img: "https://cdn.britannica.com/27/189527-050-5BAD12C2/Magic-Johnson.jpg",
+    statistics: [
+      "17,707 points",
+      "6,559 rebaunds",
+      "10,141 assist",
+      "12 All Star",
+    ],
   },
   {
-    id: 5,
-    title: "Cappadocia",
-    desc:
-      "Cappadocia… 'Kappa Tuchia', which means 'Land of Beautiful Horses' in the words of Persians. A location in Turkey and in the center of Anatolia's most popular and unique tourist centers of someone who Cappadocia, magnificent nature, valleys of unique beauty, fairy chimneys, rock-hewn settlements, millions every year with open-air museums and underground cities, local and hosts foreign tourists.",
-    image: "https://i.hurimg.com/i/hdn/75/0x0/5aa10544d3806c25541af273.jpg",
+    name: "Michael Jordan",
+    img: "https://cdn.europosters.eu/image/1300/posters/michael-jordan-i116434.jpg",
+    statistics: [
+      "32,292 points",
+      "6,672 rebaunds",
+      "5,633 assist",
+      "14 All Star",
+    ],
   },
   {
-    id: 6,
-    title: "Paris",
-    desc:
-      "Paris has been recognized as one of the most romantic cities in the world. Many famous buildings here have been added to the Unesco World Heritage List. It awaits you with its museums, fashion and historical buildings.",
-    image:
-      "https://d2rdhxfof4qmbb.cloudfront.net/wp-content/uploads/20180221130827/iStock-155381519.jpg",
+    name: "Karl Malone",
+    img: "https://i.pinimg.com/564x/49/76/10/497610981fdbe0158ba09b5c8418d87d--nba-players-basketball-players.jpg",
+    statistics: [
+      "36,928 points",
+      "14,968 rebaunds",
+      "5,248 assist",
+      "14 All Star",
+    ],
   },
   {
-    id: 7,
-    title: "Venice",
-    desc:
-      "Venice, one of Italy's indispensable stops, is known as the 'City of Channels', 'City of Waters' or 'City of Masks' thanks to its world-famous masks. If you visit in February, you can catch the Venice Carnival, where everyone wears a mask and has fun.",
-    image:
-      "https://www.qantas.com/content/travelinsider/en/explore/europe/italy/venice/things-you-need-to-know-before-you-go-to-venice/_jcr_content/parsysTop/hero.img.full.medium.jpg/1535006246068.jpg",
+    name: "Hakeem Olajuwon",
+    img: "https://cdn.artphotolimited.com/images/60913d60bd40b85323893a87/1000x1000/hakeem-olajuwon-von-den-houston-rockets.jpg",
+    statistics: [
+      "26,946 points",
+      "13,748 rebaunds",
+      "3,058 assist",
+      "12 All Star",
+    ],
   },
   {
-    id: 8,
-    title: "India",
-    desc:
-      "If you are not a vacationer but a traveler, we can say that India is the place for you. India, one of the most colorful places in the world, has a crazy variety. The Taj Mahal, the story of which you have heard more or less, is one of its famous buildings.",
-    image: "https://lp-cms-production.imgix.net/2020-11/GettyRF_494057771.jpg",
+    name: "Shaquille O'Neal",
+    img: "https://staticg.sportskeeda.com/editor/2018/10/6bb16-15398694878129-800.jpg",
+    statistics: [
+      "28,596 points",
+      "13,099 rebaunds",
+      "3,026 assist",
+      "15 All Star",
+    ],
   },
   {
-    id: 9,
-    title: "Northern Lights",
-    desc:
-      "Northern lights, the 'Aurora Borealis', named after the Dawn Goddess Aurora, are the natural glows that emerge in the sky with the interaction of the earth's magnetic field and charged particles from the Sun. This image, which makes most of us feel like in a fairy tale land, deserves to be visited at least once in a lifetime.",
-    image:
-      "https://images.unsplash.com/photo-1579033461380-adb47c3eb938?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8bm9ydGhlcm4lMjBsaWdodHN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    name: "Scottie Pippen",
+    img: "https://basketballforever.com/wp-content/uploads/2020/04/Scottie-Pippen-Chicago-Bulls.jpg",
+    statistics: [
+      "18,940 points",
+      "7,494 rebaunds",
+      "6,135 assist",
+      "7 All Star",
+    ],
   },
   {
-    id: 10,
-    title: "Fuji Mountain",
-    desc:
-      "The symbol and highest mountain of Japan, Mount Fuji is considered sacred by the Japanese and is called Fuji-San. This mountain, which has an active startovolcano, is depicted in art and photography and is a natural wonder visited by athletes and tourists.",
-    image:
-      "https://d20aeo683mqd6t.cloudfront.net/articles/title_images/000/038/943/original/pixta_43064884_S.jpg?2019&d=750x400",
+    name: "Patrick Ewing",
+    img: "https://cdn.britannica.com/37/231237-050-0CB78D66/Patrick-Ewing-New-York-Knicks-1986.jpg",
+    statistics: [
+      "24,815 points",
+      "11,607 rebaunds",
+      "2,215 assist",
+      "11 All Star",
+    ],
   },
   {
-    id: 11,
-    title: "Arashiyama Bamboo Grove",
-    desc:
-      "Bamboo Forest, or Arashiyama Bamboo Grove or Sagano Bamboo Forest, is a natural forest of bamboo in Arashiyama, Kyoto, Japan. The forest consists mostly of Moso Bamboo (Phyllostachys edulis) and consists of several pathways for tourists and visitors. The Ministry of the Environment considers it a part of the soundscape of Japan.",
-    image: "https://wallpaperaccess.com/full/534182.jpg",
+    name: "Kobe Bryant",
+    img: "https://upload.wikimedia.org/wikipedia/commons/9/96/Kobe_Bryant_8.jpg",
+    statistics: [
+      "33,643 points",
+      "7,047 rebaunds",
+      "6,306 assist",
+      "18 All Star",
+    ],
   },
   {
-    id: 12,
-    title: "Antelope Canyon",
-    desc:
-      "Antelope Canyon is a slot canyon in the American Southwest, on Navajo land east of Page, Arizona. It includes two separate, scenic slot canyon sections, referred to as Upper Antelope Canyon (or The Crack), and Lower Antelope Canyon (or The Corkscrew).",
-    image:
-      "https://images.unsplash.com/photo-1444076784383-69ff7bae1b0a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fGxvd2VyJTIwYW50ZWxvcGUlMjBjYW55b258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    name: "Charles Barkley",
+    img: "http://res.cloudinary.com/ybmedia/image/upload/c_crop,h_2000,w_1330,x_0,y_0/c_scale,f_auto,q_auto,w_700/v1/m/1/7/170254e5f61a9ce5ff77b80c824d97ba464aa254/rebounding-star-auburn.jpg",
+    statistics: [
+      "23,757 points",
+      "12,546 rebaunds",
+      "4,215 assist",
+      "11 All Star",
+    ],
   },
 ];
+
+export default data;
