e32dfe09b02aa3dc83bb960235c3d7aaffa06c2b
erhanatalan <erhanatalan2013@gmail.com>
2023-02-21 14:22:12 +0300

    headerjsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,36 @@
 
+import nba from "../../Data/nba.png";
+import styled from "styled-components";
+
+const DivStyles = styled.div` 
+  padding: 1rem;
+  text-align:center;
+  img{
+    width:300px;
+  }
+  h1{
+    color:black;
+    weight:50%;
+    font-weight:700;
+  }
+  input{
+    border-radius:8px;
+    width:40%;
+    height:40px;
+    padding:10px;
+    border: none;
+    box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+  }
+`;
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <DivStyles>
+      <img src={nba} alt="nba"/>
+      <h1 className="display-1">NBA Legends</h1>
+      <input type="search" name="" id="" placeholder="Search Player..."/>
+
+    </DivStyles>
   )
 }
 
